59e5d2c590be382df706331cdee1b1c3bc604709
fob2257 <fob2257@gmail.com>
2020-08-05 13:54:43 -0700

    Migracion m2m regresa una promesa

--- migrations/20180804183246-relation-many-to-many.js
+++ migrations/20180804183246-relation-many-to-many.js
@@ -9,28 +9,28 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-   return [
-    queryInterface.addConstraint('Usuario_LenguajePs', ['UsuarioId'], {
-      type: 'FOREIGN KEY',
-      name: 'FK_UsuarioLenguajeP_Usuario_1', // useful if using queryInterface.removeConstraint
-      references: {
-        table: 'Usuarios',
-        field: 'id',
-      },
-      onDelete: 'no action',
-      onUpdate: 'no action',
-    }),
-    queryInterface.addConstraint('Usuario_LenguajePs', ['LenguajePId'], {
-      type: 'FOREIGN KEY',
-      name: 'FK_UsuarioLenguajeP_LenguajeP_1', // useful if using queryInterface.removeConstraint
-      references: {
-        table: 'LenguajePs',
-        field: 'id',
-      },
-      onDelete: 'no action',
-      onUpdate: 'no action',
-    }),
-  ]
+    return Promise.resolve([
+      queryInterface.addConstraint('Usuario_LenguajePs', ['UsuarioId'], {
+        type: 'FOREIGN KEY',
+        name: 'FK_UsuarioLenguajeP_Usuario_1', // useful if using queryInterface.removeConstraint
+        references: {
+          table: 'Usuarios',
+          field: 'id',
+        },
+        onDelete: 'no action',
+        onUpdate: 'no action',
+      }),
+      queryInterface.addConstraint('Usuario_LenguajePs', ['LenguajePId'], {
+        type: 'FOREIGN KEY',
+        name: 'FK_UsuarioLenguajeP_LenguajeP_1', // useful if using queryInterface.removeConstraint
+        references: {
+          table: 'LenguajePs',
+          field: 'id',
+        },
+        onDelete: 'no action',
+        onUpdate: 'no action',
+      }),
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
@@ -41,9 +41,15 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-   return [
-    queryInterface.removeConstraint('Usuario_LenguajePs', 'FK_UsuarioLenguajeP_Usuario_1'),
-    queryInterface.removeConstraint('Usuario_LenguajePs', 'FK_UsuarioLenguajeP_LenguajeP_1'),
-  ]
-  }
+    return Promise.resolve([
+      queryInterface.removeConstraint(
+        'Usuario_LenguajePs',
+        'FK_UsuarioLenguajeP_Usuario_1'
+      ),
+      queryInterface.removeConstraint(
+        'Usuario_LenguajePs',
+        'FK_UsuarioLenguajeP_LenguajeP_1'
+      ),
+    ]);
+  },
 };
